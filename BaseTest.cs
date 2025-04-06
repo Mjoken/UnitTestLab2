3c6f4b8cee352b2d1b22c9dde1384a6dff7ee344
Oleg <oiksanov@yandex.ru>
2025-04-06 23:40:44 +0300

    lab2end

--- BaseTest.cs
+++ BaseTest.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace WikipediaTests
+{
+    public class BaseTest
+    {
+        protected IWebDriver driver;
+
+        [SetUp]
+        public void Setup()
+        {
+            driver = new ChromeDriver(); // Инициализация Chrome
+            driver.Manage().Window.Maximize();
+            driver.Navigate().GoToUrl("https://www.wikipedia.org/");
+        }
+
+        [TearDown]
+        public void Teardown()
+        {
+            driver.Quit(); // Закрытие браузера после теста
+        }
+    }
+}
